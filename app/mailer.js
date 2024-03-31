337343550428d90e5911de1a459beab08566fe60
Daniel Cohen <daniel@codeinvain.com>
2024-03-31 13:13:40 +0300

    bcc support

--- app/mailer.js
+++ app/mailer.js
@@ -1,11 +1,11 @@
 const nodemailer = require('nodemailer');
 
-function send(html, options = {}){
-  return new Promise(function(resolve, reject){
+function send(html, options = {}) {
+  return new Promise(function (resolve, reject) {
     let smtpConfig = {
       host: process.env.SMTP_HOST,
       port: process.env.SMTP_PORT || 587,
-      secure: process.env.SMTP_SECURE == '1', 
+      secure: process.env.SMTP_SECURE == '1',
       auth: {
         user: process.env.SMTP_AUTH_USER,
         pass: process.env.SMTP_AUTH_PASS
@@ -13,16 +13,16 @@ function send(html, options = {}){
     };
     let transporter = nodemailer.createTransport(smtpConfig);
 
-    let subject = typeof(options.subject) == 'function' ? options.subject(html, options)  : options.subject;
+    let subject = typeof (options.subject) == 'function' ? options.subject(html, options) : options.subject;
     // setup email data with unicode symbols
-    let mailOptions = Object.assign({subject}, (({from, to})=>({from, to}))(options));
+    let mailOptions = Object.assign({ subject }, (({ from, to, bcc }) => ({ from, to, bcc }))(options));
     log.debug(mailOptions);
     mailOptions.html = html;
     // send mail with defined transport object
     transporter.sendMail(mailOptions, (error, info) => {
       if (error) {
         let msg = 'failed sending email';
-        log.warn({error}, msg);
+        log.warn({ error }, msg);
         reject(msg, error);
       } else {
         let msg = `Message sent: ${info.messageId}`;
@@ -36,4 +36,4 @@ function send(html, options = {}){
 
 module.exports = {
   send
-} ;
+};
